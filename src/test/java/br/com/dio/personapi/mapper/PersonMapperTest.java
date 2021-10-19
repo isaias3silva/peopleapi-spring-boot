7d10dd8e89e70ff89e705a75ad13478ee229ee0b
isaias3silva <52718665+isaias3silva@users.noreply.github.com>
2021-10-19 20:05:11 -0300

    Create unit tests PersonMapperTest.java.

---
+++ src/test/java/br/com/dio/personapi/mapper/PersonMapperTest.java
@@ -0,0 +1,52 @@
+package br.com.dio.personapi.mapper;
+
+import br.com.dio.personapi.mapper.PersonMapper;
+import br.com.dio.personapi.dto.request.PersonDTO;
+import br.com.dio.personapi.dto.request.PhoneDTO;
+import br.com.dio.personapi.entity.Person;
+import br.com.dio.personapi.entity.Phone;
+import br.com.dio.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonMapperTest {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Test
+    void testGivenPersonDTOThenReturnPersonEntity() {
+        PersonDTO personDTO = PersonUtils.createFakeDTO();
+        Person person = personMapper.toModel(personDTO);
+
+        assertEquals(personDTO.getFistName(), person.getFistName());
+        assertEquals(personDTO.getLastName(), person.getLastName());
+        assertEquals(personDTO.getCpf(), person.getCpf());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phoneDTO.getType(), phone.getType());
+        assertEquals(phoneDTO.getNumber(), phone.getNumber());
+    }
+
+    @Test
+    void testGivenPersonEntityThenReturnPersonDTO() {
+        Person person = PersonUtils.createFakeEntity();
+        PersonDTO personDTO = personMapper.toDTO(person);
+
+        assertEquals(person.getFistName(), personDTO.getFistName());
+        assertEquals(person.getLastName(), personDTO.getLastName());
+        assertEquals(person.getCpf(), personDTO.getCpf());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phone.getType(), phoneDTO.getType());
+        assertEquals(phone.getNumber(), phoneDTO.getNumber());
+    }
+}
